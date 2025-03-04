7378eb9fa66c99449d0e639339266fc820e5aae0
András Petrik <petrik.andras@gmail.com>
2025-03-04 15:33:25 +0100

    initial sceleton of the project

---
+++ src/test/java/com/test/movie/movietest/MovietestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.movie.movietest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovietestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
