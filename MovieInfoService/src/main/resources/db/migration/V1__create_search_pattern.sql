33e037ed4cd826740b27ac28da3c1f56d200e11f
András Petrik <petrik.andras@gmail.com>
2025-03-07 17:45:59 +0100

    remove unnecessary sql

--- MovieInfoService/src/main/resources/db/migration/V1__create_search_pattern.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE search_pattern (
-                                   id serial primary key ,
-                                   created datetime NOT NULL,
-                                   title varchar(255) NOT NULL,
-                                   api_name varchar(50) DEFAULT NULL,
-                                   page_number int DEFAULT NULL
-);
